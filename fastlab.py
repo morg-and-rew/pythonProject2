48fee143a8350657aa766ac96e3b9fc60fab5bb5
morg-and-rew <andreym67764@gmail.com>
2024-05-26 20:53:23 +0700

    Merge remote-tracking branch 'origin/master'

--- fastlab.py
+++ fastlab.py
@@ -30,7 +30,7 @@ async def make_image(request: Request,
                      angle: int = Form(),
                      files: List[UploadFile] = File(description="Multiple files as UploadFile"),
                      resp: str = Form()):
-    recaptcha_secret = "6LftbOgpAAAAAC8YIIB3p2x0s58eEnrzx"  # Замените на ваш секретный ключ reCAPTCHA
+    recaptcha_secret = "6LftbOgpAAAAAC8YIIB3p2x0s58eEnrzx-5Sw9t3"  # Замените на ваш секретный ключ reCAPTCHA
     recaptcha_data = {
         'secret': recaptcha_secret,
         'response': resp
