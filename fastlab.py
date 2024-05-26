61b4171e7b4347bf9e45e60eba4797854932df47
morg-and-rew <andreym67764@gmail.com>
2024-05-26 20:43:04 +0700

    Merge remote-tracking branch 'origin/master'

--- fastlab.py
+++ fastlab.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request, Form, HTTPException, File, UploadFile
 from typing import List
 import hashlib
-import numpy as np
+import numpy
 import io
 import requests
 from PIL import Image
@@ -14,19 +14,23 @@ from pathlib import Path
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+def sum_two_args(x,y):
+ return x+y
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+#1
 @app.post("/image_form", response_class=HTMLResponse)
 async def make_image(request: Request,
-                     noise_level: float = Form(),  # Добавлен уровень шума
-                     angle: float = Form(),  # Добавлен угол
+                     angle: int = Form(),
                      files: List[UploadFile] = File(description="Multiple files as UploadFile"),
                      resp: str = Form()):
-    recaptcha_secret = "6LftbOgpAAAAAC8YIIB3p2x0s58eEnrzx-5Sw9t3"
+    recaptcha_secret = "6LftbOgpAAAAAC8YIIB3p2x0s58eEnrzx"  # Замените на ваш секретный ключ reCAPTCHA
     recaptcha_data = {
         'secret': recaptcha_secret,
         'response': resp
@@ -34,6 +38,7 @@ async def make_image(request: Request,
     recaptcha_url = "https://www.google.com/recaptcha/api/siteverify"
     recaptcha_verification = requests.post(recaptcha_url, data=recaptcha_data)
     recaptcha_result = recaptcha_verification.json()
+
     if not recaptcha_result['success']:
         raise HTTPException(status_code=400, detail="Ошибка проверки капчи")
 
@@ -42,59 +47,62 @@ async def make_image(request: Request,
     if len(files) > 0:
         if len(files[0].filename) > 0:
             ready = True
-
     images = []
     original_histogram_images = []
-    noisy_histogram_images = []
-
+    rotated_histogram_images = []
     if ready:
         print([file.filename.encode('utf-8') for file in files])
         images = ["static/" + hashlib.sha256(file.filename.encode('utf-8')).hexdigest() for file in files]
+
         content = [await file.read() for file in files]
+
         p_images = [Image.open(io.BytesIO(con)).convert("RGB") for con in content]
+
         for i in range(len(p_images)):
-            original_histogram = get_histogram(p_images[i])
-
-            # Добавление шума к изображению
-            noise = np.random.normal(0, noise_level, p_images[i].size)  # Генерация шума
-            noisy_image = np.clip(p_images[i] + noise, 0, 255).astype(np.uint8)  # Добавление шума к изображению
-            noisy_histogram = get_histogram(noisy_image)
-
             rotated_image = p_images[i].rotate(angle, expand=True)
+
+            original_histogram = get_histogram(p_images[i])
+            rotated_histogram = get_histogram(rotated_image)
+
             rotated_image.save("./" + images[i], 'JPEG')
 
             original_histogram_image = create_histogram_image(original_histogram)
-            noisy_histogram_image = create_histogram_image(noisy_histogram)
+            rotated_histogram_image = create_histogram_image(rotated_histogram)
 
             original_histogram_image_path = f"static/original_histogram_{i}.png"
-            noisy_histogram_image_path = f"static/noisy_histogram_{i}.png"
-            original_histogram_images.append(original_histogram_image_path)
-            noisy_histogram_images.append(noisy_histogram_image_path)
+            rotated_histogram_image_path = f"static/rotated_histogram_{i}.png"
+            original_histogram_image.save(original_histogram_image_path)
+            rotated_histogram_image.save(rotated_histogram_image_path)
 
-    return templates.TemplateResponse("forms.html", {"request": request, "ready": ready, "images": images,
-                                                     "original_histogram_images": original_histogram_images,
-                                                     "noisy_histogram_images": noisy_histogram_images})
+            original_histogram_images.append(original_histogram_image_path)
+            rotated_histogram_images.append(rotated_histogram_image_path)
+
+        return templates.TemplateResponse("forms.html", {"request": request, "ready": ready, "images": images,
+                                                         "original_histogram_images": original_histogram_images,
+                                                         "rotated_histogram_images": rotated_histogram_images})
+
 
 def get_histogram(image):
-    pixels = np.array(image)
+    pixels = numpy.array(image)
 
-    r_histogram, r_bins = np.histogram(pixels[:, :, 0].flatten(), bins=np.arange(0, 257, 10))
-    g_histogram, g_bins = np.histogram(pixels[:, :, 1].flatten(), bins=np.arange(0, 257, 10))
-    b_histogram, b_bins = np.histogram(pixels[:, :, 2].flatten(), bins=np.arange(0, 257, 10))
+    r_histogram, r_bins = numpy.histogram(pixels[:, :, 0].flatten(), bins=numpy.arange(0, 257, 10))
+    g_histogram, g_bins = numpy.histogram(pixels[:, :, 1].flatten(), bins=numpy.arange(0, 257, 10))
+    b_histogram, b_bins = numpy.histogram(pixels[:, :, 2].flatten(), bins=numpy.arange(0, 257, 10))
 
     max_length = max(len(r_histogram), len(g_histogram), len(b_histogram))
-    r_histogram = np.pad(r_histogram, (0, max_length - len(r_histogram)), mode='constant')
-    g_histogram = np.pad(g_histogram, (0, max_length - len(g_histogram)), mode='constant')
-    b_histogram = np.pad(b_histogram, (0, max_length - len(b_histogram)), mode='constant')
+    r_histogram = numpy.pad(r_histogram, (0, max_length - len(r_histogram)), mode='constant')
+    g_histogram = numpy.pad(g_histogram, (0, max_length - len(g_histogram)), mode='constant')
+    b_histogram = numpy.pad(b_histogram, (0, max_length - len(b_histogram)), mode='constant')
 
     return r_histogram.tolist(), g_histogram.tolist(), b_histogram.tolist()
+
 
 def create_histogram_image(histograms):
     fig, ax = plt.subplots(figsize=(8, 6))
 
-    ax.bar(np.arange(0, len(histograms[0]) * 10, 10), histograms[0], color='r', width=10, label='Red')
-    ax.bar(np.arange(0, len(histograms[1]) * 10, 10), histograms[1], color='g', width=10, label='Green')
-    ax.bar(np.arange(0, len(histograms[2]) * 10, 10), histograms[2], color='b', width=10, label='Blue')
+    ax.bar(numpy.arange(0, len(histograms[0]) * 10, 10), histograms[0], color='r', width=10, label='Red')
+    ax.bar(numpy.arange(0, len(histograms[1]) * 10, 10), histograms[1], color='g', width=10, label='Green')
+    ax.bar(numpy.arange(0, len(histograms[2]) * 10, 10), histograms[2], color='b', width=10, label='Blue')
     ax.set_title('Гистограмма распределения цветов')
     ax.set_xlabel('Значение пиксилей')
     ax.set_ylabel('Частота')
@@ -106,6 +114,7 @@ def create_histogram_image(histograms):
     plt.close()
     buf.seek(0)
     return Image.open(buf)
+
 
 @app.get("/image_form", response_class=HTMLResponse)
 async def make_image(request: Request):
